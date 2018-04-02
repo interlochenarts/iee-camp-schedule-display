5d0495b5784870f03747dadccb3ab089d44e5004
Mike Smith <mike@noiseparticles.com>
2018-04-02 15:29:15 -0400

    grid displays free periods in appropriate places

--- src/app/matrix/matrix.component.ts
+++ src/app/matrix/matrix.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input, OnChanges, OnInit, ViewChild, ViewContainerRef} from '@angular/core';
+import {Component, ElementRef, Input, OnChanges, OnInit, Renderer2, ViewChild, ViewContainerRef} from '@angular/core';
 import {ScheduleCourse} from '../_classes/ScheduleCourse';
 
 @Component({
@@ -8,25 +8,33 @@ import {ScheduleCourse} from '../_classes/ScheduleCourse';
 })
 export class MatrixComponent implements OnInit, OnChanges {
   @Input() termSchedule: ScheduleCourse[];
-  @ViewChild('matrixContainer', {read: ViewContainerRef}) matrixContainer: ViewContainerRef;
+  @ViewChild('matrixContainer') matrixContainer: ElementRef;
+
+  divs: HTMLDivElement[] = [];
   firstDay = 7;
   lastDay = -1;
-  style: string;
 
   periodNumbers: number[] = [];
 
+  freePeriods: ScheduleCourse[] = [];
+
   dayNames = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+
+  gridPositionsFilled: boolean[] = [];
 
   getGridColumnStyle(): string {
     const days = this.lastDay - this.firstDay + 1;
-    this.style = 'grid-template-columns: 0.75fr repeat(' + days + ', 1fr);';
-    return this.style;
+    return 'grid-template-columns: 0.75fr repeat(' + days + ', 1fr);';
   }
 
   get periodDays(): number[] {
     return Array.from(
       {length: (this.lastDay - this.firstDay + 1)},
       (v, k) => k + this.firstDay);
+  }
+
+  private get dayCount(): number {
+    return this.lastDay - this.firstDay + 1;
   }
 
   getHeaderGridColumnStyle(index: number): string {
@@ -45,19 +53,25 @@ export class MatrixComponent implements OnInit, OnChanges {
     return 'grid-column: ' + (index + 1) + ' / ' + (index + 1) + ';';
   }
 
-  constructor() {
+  constructor(private renderer: Renderer2) {
   }
 
   ngOnInit() {
-    this.updateSchedule();
   }
 
   ngOnChanges() {
+    this.divs.forEach(div => {
+      this.renderer.removeChild(this.matrixContainer.nativeElement, div);
+    });
     this.updateSchedule();
   }
 
   updateSchedule(): void {
     if (this.termSchedule) {
+      this.divs.length = 0;
+      this.periodNumbers.length = 0;
+      this.firstDay = 7;
+      this.lastDay = -1;
       this.termSchedule.forEach(c => {
         // console.log('name: ' + c.courseName + ' / days: ' + c.days.join() + ' / periods: ' + c.periods.join());
         c.periods.forEach(p => {
@@ -78,17 +92,82 @@ export class MatrixComponent implements OnInit, OnChanges {
 
       this.periodNumbers.sort();
 
-      this.createDivsForCourse(c);
+      this.gridPositionsFilled = Array.from(
+        {length: (this.dayCount) * this.periodNumbers[this.periodNumbers.length - 1]},
+        () => {
+          return false;
+        });
+
+      this.termSchedule.forEach(c => {
+        this.createDivsForCourse(c);
+      });
+
+      // create Free Period course divs
+      this.freePeriods = this.createFreePeriods();
+      this.freePeriods.forEach(free => {
+        this.createDivsForCourse(free);
+      });
+
+      this.divs.forEach(div => {
+        this.renderer.appendChild(this.matrixContainer.nativeElement, div);
+      });
     }
   }
 
-  createDivsForCourse(course: ScheduleCourse) {
-    course.periods.forEach(p => {
-      if (this.courseIsFirstInRange(course, p)) {
-        // insert div
-        
+  createFreePeriods(): ScheduleCourse[] {
+    const freePs: ScheduleCourse[] = [];
+
+    this.gridPositionsFilled.forEach((filled, index) => {
+      if (filled === false) {
+        const periodIndex = Math.floor(index / this.dayCount);
+        const dayIndex = index - (periodIndex * this.dayCount);
+
+        let course = freePs[dayIndex];
+        if (!course) {
+          course = new ScheduleCourse('Free Period');
+          course.schedulePeriods = this.periodNumbers[periodIndex] + '';
+          course.scheduleDays = (dayIndex + 1) + '';
+        } else {
+          course.schedulePeriods += ',' + this.periodNumbers[periodIndex] + '';
+          course.scheduleDays += ',' + (dayIndex + 1) + '';
+        }
+
+        freePs[dayIndex] = course;
       }
     });
+
+    return freePs;
+  }
+
+  createDivsForCourse(course: ScheduleCourse) {
+    course.days.forEach(d => {
+      course.periods.forEach(p => {
+        if (this.courseIsFirstInRange(course, p)) {
+          // insert div
+          const div: HTMLDivElement = <HTMLDivElement>document.createElement('div');
+          div.style['grid-row'] = (p + 1) + ' / ' + (this.lastPeriodInRange(course, p) + 2);
+          div.style['grid-column'] = (d + 1) + ' / ' + (d + 2);
+          div.style.padding = '5px';
+          div.style.borderBottom = '1px solid gray';
+          if (d !== this.firstDay) {
+            div.style.borderLeft = '1px solid gray';
+          }
+          if (d === this.lastDay) {
+            div.style.borderRight = '1px solid gray';
+          }
+          div.innerHTML = '<p><strong>' + course.courseName + '</strong></p>' +
+            (course.instructor ? ('<p>' + course.instructor + '</p>') : '') +
+            (course.location ? ('<p>' + course.location + '</p>') : '');
+
+          this.divs.push(div);
+        }
+
+        // set position filled
+        const gridPosition: number = (this.periodNumbers.indexOf(p) * this.dayCount) + this.periodDays.indexOf(d);
+        this.gridPositionsFilled[gridPosition] = true;
+      });
+    });
+
   }
 
   lastPeriodInRange(course: ScheduleCourse, period: number): number {
