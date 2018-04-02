862587e03f725e4241dec836d88d242313e7d783
Mike Smith <mike@noiseparticles.com>
2018-04-02 15:42:02 -0400

    add times to period column

--- src/app/matrix/matrix.component.ts
+++ src/app/matrix/matrix.component.ts
@@ -15,6 +15,7 @@ export class MatrixComponent implements OnInit, OnChanges {
   lastDay = -1;
 
   periodNumbers: number[] = [];
+  periodTimes = ['8 am', '9 am', '10 am', '11 am', '12 pm', '1 pm', '2 pm', '3 pm', '4 pm', '5 pm'];
 
   freePeriods: ScheduleCourse[] = [];
 
