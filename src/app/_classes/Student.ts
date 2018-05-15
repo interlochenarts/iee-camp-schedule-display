07069fd5efa0c663446bf84b1a23a454065278c4
Mike Smith <smithmd@interlochen.org>
2018-05-15 12:21:53 -0400

    add checks to suppress errors when data isn't finished loading

--- src/app/_classes/Student.ts
+++ src/app/_classes/Student.ts
@@ -18,17 +18,25 @@ export class Student {
   }
 
   get housingDivision() {
-    return this.cabin.split('-')[0];
+    if (this.cabin) {
+      return this.cabin.split('-')[0];
+    }
+
+    return '';
   }
 
   public getMajorBySessionName(session: string): string {
-    let major = this.majorBySessionName[session];
+    if (this.majorBySessionName) {
+      let major = this.majorBySessionName[session];
 
-    if (!major) {
-      // try to find a major in the 6 or 4 week sessions
-      major = this.majorBySessionName['6 weeks'] || this.majorBySessionName['1st 4 weeks'] || this.majorBySessionName['2nd 4 weeks'];
+      if (!major) {
+        // try to find a major in the 6 or 4 week sessions
+        major = this.majorBySessionName['6 weeks'] || this.majorBySessionName['1st 4 weeks'] || this.majorBySessionName['2nd 4 weeks'];
+      }
+
+      return major;
     }
 
-    return major;
+    return '';
   }
 }
