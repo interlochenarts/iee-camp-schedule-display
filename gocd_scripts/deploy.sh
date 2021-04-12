ddee038afb4b397d316cf08a4bf8e300bed3af77
Mike Smith <smithmd@interlochen.org>
2021-04-12 14:54:23 -0400

    moved key file to var

--- gocd_scripts/deploy.sh
+++ gocd_scripts/deploy.sh
@@ -16,5 +16,9 @@ if [[ ! -z ${LOGIN_SERVER} ]]; then
   echo -e "Missing DX_ENV environment variable"
 fi
 
-sfdx force:auth:jwt:grant -i${SFDC_TOKEN} -f/home/wwadmin/certificates/server.key -u${sfdcUser} -a${DX_ENV} -rhttps://${LOGIN_SERVER}.salesforce.com
+if [[ ! -z ${KEY_FILE} ]]; then
+  echo -e "Missing KEY_FILE environment variable"
+fi
+
+sfdx force:auth:jwt:grant -i${SFDC_TOKEN} -f/home/wwadmin/certificates/${KEY_FILE} -u${sfdcUser} -a${DX_ENV} -rhttps://${LOGIN_SERVER}.salesforce.com
 sfdx force:mdapi:deploy -dSalesforce/src -u${DX_ENV} -w60
