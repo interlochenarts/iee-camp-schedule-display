04b3740a49b526fffb336e99b8eec28705b3dbbd
msmith <smithmd@interlochen.org>
2023-05-12 14:27:00 -0400

    fix build scripts

--- gocd_scripts/deploy.sh
+++ gocd_scripts/deploy.sh
@@ -20,5 +20,19 @@ if [[ -z ${KEY_FILE} ]]; then
   echo -e "Missing KEY_FILE environment variable"
 fi
 
-sfdx force:auth:jwt:grant -i${SFDC_CONSUMER_KEY} -f/home/wwadmin/certificates/${KEY_FILE} -u${sfdcUser} -a${DX_ENV} -rhttps://${LOGIN_SERVER}.salesforce.com
+if [[ -z ${SFDX_CLI_VERSION} ]]; then
+  echo -e "Missing SFDX_CLI_VERSION environment variable"
+fi
+
+source "$HOME/.nvm/nvm.sh"
+nvm install --lts
+
+echo -e "\n===> SFDX Update <===\n"
+npm install -g sfdx-cli@${SFDX_CLI_VERSION}
+sfdx --version
+
+echo -e "sfdx force:auth:jwt:grant -i${SFDC_CONSUMER_KEY} -f/home/wwadmin/certificates/${KEY_FILE} -u${sfdcUser} -a${DX_ENV} -r${LOGIN_SERVER}"
+sfdx force:auth:jwt:grant -i${SFDC_CONSUMER_KEY} -f/home/wwadmin/certificates/${KEY_FILE} -u${sfdcUser} -a${DX_ENV} -r${LOGIN_SERVER}
+
+echo -e "sfdx force:mdapi:deploy -dSalesforce/src -u${DX_ENV} -w60"
 sfdx force:mdapi:deploy -dSalesforce/src -u${DX_ENV} -w60
